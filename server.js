67097981bb9c54ad012e4b232d4acbf9f9bd9846
邱勇 <yq@yyqdeMacBook-Pro.local>
2018-12-16 23:09:40 +0800

    no message

---
+++ server.js
@@ -0,0 +1,25 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不好？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var path = request.url 
+  var query = ''
+  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
+  var pathNoQuery = parsedUrl.pathname
+  var queryObject = parsedUrl.query
+  var method = request.method
+
+
+
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请在浏览器中打开 http://localhost:' + port)

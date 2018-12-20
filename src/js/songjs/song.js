1e6bb7abec2fc96160f2acbc949dd39bc89665dd
邱勇 <yq@yyqdeMacBook-Pro.local>
2018-12-20 21:41:59 +0800

    no message

--- src/js/songjs/song.js
+++ src/js/songjs/song.js
@@ -1,30 +1,57 @@
 {
     let view = {
-        el:'#app',
-        init(){
-          this.$el = $(this.el)
+        el: '#app',
+        init() {
+            this.$el = $(this.el)
         },
-        template:`
+        template: `
         `,
-        render(){
+        render() {
 
         },
-        setBG(data){
-            this.$el.append(`<style>.songPage::before{background:url(${data.cover}) no-repeat center center;background-size: cover;}</style>`);
+        setBG(data) {
+            this.$el.append(`
+                 <style>
+                 .songPage::before
+                 {
+                     background:url(${data.cover}) no-repeat center center;
+                     background-size: cover;
+                 }
+                 </style>
+`           );
+            this.$el.find('.cover').append(`
+                <style>
+                .cover
+                {
+                    background:url(${data.cover}) no-repeat center center;
+                    background-size: cover;
+                }
+                </style>
+            `)
+        },
+        play(data){
+            this.$el.find('span>img').removeClass('active')
+            let audio = this.$el.find('audio')[0]
+            audio.src = data.url
+            audio.play()
+        },
+        pause(){
+            this.$el.find('span>img').addClass('active')
         }
+
     }
     let model = {
-        data:{
-            song:{
-                song:'',
-                singer:'',
-                url:'',
-                id:'',
-                lyrics:'',
-                cover:''
+        data: {
+            song: {
+                song: '',
+                singer: '',
+                url: '',
+                id: '',
+                lyrics: '',
+                cover: ''
             }
         },
-        getURL(id){
+        getURL(id) {
             var query = new AV.Query('Songs');
             return query.get(id).then((song) => {
                 // 成功获得实例
@@ -36,21 +63,19 @@
         }
     }
     let controller = {
-        init(view,model){
+        init(view, model) {
             this.view = view
             this.model = model
             this.view.init()
             this.bindEvents()
         },
-        bindEvents(){
+        bindEvents() {
             let id = window.location.search.split('=')[1]
-            this.model.getURL(id).then(()=>{
-                let audio = this.view.$el.find('audio')[0]
-                audio.src = this.model.data.song.url
-                // audio.play()
+            this.model.getURL(id).then(() => {
                 this.view.setBG(this.model.data.song)
+                this.view.play(this.model.data.song)
             })
         }
     }
-    controller.init(view,model)
+    controller.init(view, model)
 }
